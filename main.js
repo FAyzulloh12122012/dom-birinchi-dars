0c9f2ab10f04ae9be5e62fa0930e540f26698654
FAyzulloh12122012 <13091988@gmail.com>
2025-12-15 16:54:27 +0500

    dom birinchi

---
+++ main.js
@@ -0,0 +1,4 @@
+const conteyner = document.querySelector('.cointeyner');
+console.log(conteyner.innerHTML); // teg
+console.log(conteyner.textContent); // text
+console.log(conteyner.innerText)// tashalgan jaoylani olib tashlaydi
