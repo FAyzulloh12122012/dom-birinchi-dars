26863e10546f4b673fbbd861b56e5b6c6675d036
FAyzulloh12122012 <13091988@gmail.com>
2025-12-16 15:25:31 +0500

    dom

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const conteyner = document.querySelector('.cointeyner');
 console.log(conteyner.innerHTML); // teg
 console.log(conteyner.textContent); // text
-console.log(conteyner.innerText)// tashalgan jaoylani olib tashlaydi
+console.log(conteyner.innerText);// tashalgan jaoylani olib tashlaydi
 
-const item1 = document.getElementsByClassName('item1');
-console.log(item1);
+const item0 = document.querySelector('.item0');
+console.log(item0.innerText);
 
-const item2 = document.getElementsByClassName('item2');
-console.log(item2);
+const item1 = document.querySelector('.item1');
+console.log(item1.innerText);
 
-const item3 = document.getElementsByClassName('item3');
-console.log(item3);
+const item2 = document.querySelector('.item2');
+console.log(item2.innerText);
 
-const item4 = document.getElementsByClassName('item4');
-console.log(item4);
+const item3 = document.querySelector('.item3');
+console.log(item3.innerText);
