357ea889ae03d84735ea0338dce6043d2826d123
FAyzulloh12122012 <13091988@gmail.com>
2025-12-16 15:08:54 +0500

    dom

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ const conteyner = document.querySelector('.cointeyner');
 console.log(conteyner.innerHTML); // teg
 console.log(conteyner.textContent); // text
 console.log(conteyner.innerText)// tashalgan jaoylani olib tashlaydi
+
+const item1 = document.getElementsByClassName('item1');
+console.log(item1);
+
+const item2 = document.getElementsByClassName('item2');
+console.log(item2);
+
+const item3 = document.getElementsByClassName('item3');
+console.log(item3);
+
+const item4 = document.getElementsByClassName('item4');
+console.log(item4);
