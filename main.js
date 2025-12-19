8a032e531b1aead8009ecf33c6b142f19b938f52
FAyzulloh12122012 <13091988@gmail.com>
2025-12-19 11:00:36 +0500

    function

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const conteyner = document.querySelector('.cointeyner');
-console.log(conteyner.innerHTML); // teg
-console.log(conteyner.textContent); // text
-console.log(conteyner.innerText);// tashalgan jaoylani olib tashlaydi
+// // const conteyner = document.querySelector('.cointeyner');
+// // console.log(conteyner.innerHTML); // teg
+// // console.log(conteyner.textContent); // text
+// // console.log(conteyner.innerText);// tashalgan jaoylani olib tashlaydi
 
-const item0 = document.querySelector('.item0');
-console.log(item0.innerText);
+// // const item0 = document.querySelector('.item0');
+// // console.log(item0.innerText);
 
-const item1 = document.querySelector('.item1');
-console.log(item1.innerText);
+// // const item1 = document.querySelector('.item1');
+// // console.log(item1.innerText);
 
-const item2 = document.querySelector('.item2');
-console.log(item2.innerText);
+// // const item2 = document.querySelector('.item2');
+// // console.log(item2.innerText);
 
-const item3 = document.querySelector('.item3');
-console.log(item3.innerText);
+// // const item3 = document.querySelector('.item3');
+// // console.log(item3.innerText);
+
+// function salom(matin){
+//     console.log(`Assalomu aleykum ${matin}`);
+// }
+// salom("Fayzulloh")
+
+// function language(go = "good afternun"){
+//     console.log(go);
+// }
+// language()
+// language("hi")
+// language("hallo")
+// language("Assalomu aleykum")
+
+function hallo(a = "Kimdir"){ // a bu parametor
+    console.log(`Hallo ${a}`);
+    return;
+}
+hallo(); // qrgument
